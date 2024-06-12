b514994d75de6aeaa76a2fbc0bf0fad8c025e3c0
nadezhdaigumenova <nadya.kyzlakova@mail.ru>
2024-06-12 21:54:45 +0700

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = 10
+second = 15
+third = 20
+if first == second:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+elif first != second != third:
+    print(0)
+    
